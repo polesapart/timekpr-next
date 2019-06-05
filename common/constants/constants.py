379a9dbe460b833f4c12a1a65db4dbd99d7ffa94
Eduards <edzis@inbox.lv>
2019-06-05 17:10:18 +0000

    Helping poor gnome3 users who can not distinguish between multiple similar longer application launcher titles due to shell shortening the names

--- common/constants/constants.py
+++ common/constants/constants.py
@@ -15,7 +15,7 @@ from datetime import datetime
 
 # ## constants ##
 # version (in case config is corrupt or smth like that)
-TK_VERSION = "0.2.4"
+TK_VERSION = "0.2.5"
 TK_DEV_ACTIVE = False  # change this accordingly when running in DEV or PROD
 TK_DEV_BUS = "ses"  # this sets up which bus to use for development (sys or ses)
 
