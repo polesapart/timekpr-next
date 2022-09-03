9413d722fbb2583057b68be00673856599e1350a
Eduards Bezverhijs <edzis@inbox.lv>
2022-09-03 23:36:29 +0300

    notification changes in client + more logging

--- common/constants/constants.py
+++ common/constants/constants.py
@@ -46,7 +46,7 @@ TK_LOG_OWNER_ADMIN_SU = 3
 TK_LOG_AUTO_FLUSH_EVT_CNT = 42
 
 # client config and default values
-TK_CL_NOTIF_MAX = 30
+TK_CL_NOTIF_MAX = 60
 TK_CL_NOTIF_TMO = 3
 TK_CL_NOTIF_CRIT_TMO = 10
 TK_CL_NOTIF_SND_FILE_WARN = "/usr/share/sounds/freedesktop/stereo/dialog-information.oga"
